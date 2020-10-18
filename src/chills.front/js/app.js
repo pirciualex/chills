67f17ace00dd85fa2e1a2b0b4da6270923c9998b
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-10-18 19:57:52 +0300

    Add all posts on the home page

--- src/chills.front/js/app.js
+++ src/chills.front/js/app.js
@@ -1,27 +1,22 @@
 import { Events } from "./events.js";
 import { Observer } from "./observer.js";
-import { API } from "./api.js";
 import { UI } from "./ui.js";
 
 const events = new Events();
 const observer = new Observer();
-const api = new API();
 const ui = new UI();
 
 document.addEventListener("DOMContentLoaded", onload);
 
 async function onload(e) {
-  const posts = await api.getAllPosts();
-  const post = await api.getSinglePost("odd-is-good");
-  console.log(posts);
-  console.log(post);
+    const topP = await ui.renderTopPosts();
+    const allP = await ui.renderAllPosts();
+    topPosts.appendChild(topP);
+    document.querySelector("#posts").appendChild(allP);
 
-  const topP = await ui.renderTopPosts();
-  topPosts.appendChild(topP);
+    events.menuIconEvent();
+    events.searchIconEvent();
+    events.imageLinksEvent();
 
-  events.menuIconEvent();
-  events.searchIconEvent();
-  events.imageLinksEvent();
-
-  observer.observeHeader();
+    observer.observeHeader();
 }
